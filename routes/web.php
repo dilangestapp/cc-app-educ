6325627867d7dbd7862444362ebaa0b516f9635e
cabreltech <nguentouoc@gmail.com>
2025-12-29 15:00:32 +0100

    Fix duplicate Route import

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
 });
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('home');
-});
-use Illuminate\Support\Facades\DB;
 
 Route::get('/db-test', function () {
     return DB::table('migrations')->get();
