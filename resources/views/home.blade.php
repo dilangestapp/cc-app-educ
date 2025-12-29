556fc17bc080f2b2fcc96e3c8551c101ef0672fa
cabreltech <nguentouoc@gmail.com>
2025-12-29 15:22:33 +0100

    Add auth (Breeze)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CC App Educ</title>
+</head>
+<body>
+    <h1>CC App Educ – Plateforme active</h1>
+    <p>Application opérationnelle.</p>
+</body>
+</html>
