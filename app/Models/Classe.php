6d61f41014fe2131238b0871279b94e5209c35dc
cabreltech <nguentouoc@gmail.com>
2025-12-30 13:34:47 +0100

    Fix Classe model (add matieres relationship)

--- app/Models/Classe.php
+++ app/Models/Classe.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Classe extends Model
 {
     use HasFactory;
+
+    protected $table = 'classes';
+
+    // Mets ici les champs réellement présents dans ta table classes
+    protected $fillable = [
+        'nom',
+    ];
+
+    /**
+     * Une classe peut avoir plusieurs matières (pivot classe_matiere)
+     */
+    public function matieres()
+    {
+        return $this->belongsToMany(
+            \App\Models\Matiere::class,
+            'classe_matiere',
+            'classe_id',
+            'matiere_id'
+        )->withTimestamps();
+    }
 }
