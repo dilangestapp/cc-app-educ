2d6bc7b6dfd62658b028c24a621f69e482699069
cabreltech <nguentouoc@gmail.com>
2025-12-30 09:55:26 +0100

    Add Classes module (controller, model, views)

---
+++ resources/views/classes/index.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold">Classes</h2>
+    </x-slot>
+
+    <div class="p-6">
+        <p>Page Classes OK</p>
+    </div>
+</x-app-layout>
